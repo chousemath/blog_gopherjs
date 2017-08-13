ecb0862be3bcbf5936dab5fed7196f4e90cc1fd2
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-13 11:35:52 +0900

    testing deployment again

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ NC='\033[0m'
 
 echo "Type your commit comment, followed by [ENTER]:"
 read comment
-echo "${GREEN}Commit comment has been loaded${NC}"
+echo -e "${GREEN}Commit comment has been loaded${NC}"
 git add .
-echo "${GREEN}git add . was performed${NC}"
+echo -e "${GREEN}git add . was performed${NC}"
 eval "git commit -am \"${comment}\""
-echo "${GREEN}git commit -am was performed${NC}"
+echo -e "${GREEN}git commit -am was performed${NC}"
 git push
-echo "${GREEN}git push was performed${NC}"
-echo "${GREEN}deployment complete!${NC}"
+echo -e "${GREEN}git push was performed${NC}"
+echo -e "${GREEN}deployment complete!${NC}"
