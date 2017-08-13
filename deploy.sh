7bf9c8e8a33dae948a928d43890d9fa090c40793
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-13 11:38:54 +0900

    testing deployment one last time

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,23 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+LIGHTCYAN='\033[1;36m'
 NC='\033[0m'
 
 echo "Type your commit comment, followed by [ENTER]:"
+# the read command stores the user input as a string
 read comment
-echo -e "${GREEN}Commit comment has been loaded${NC}"
+# the -e command enables the backslash necessary for colored text
+echo -e "${GREEN}commit comment has been loaded${NC}"
+# just a spacer
+echo -e "${LIGHTCYAN}............${NC}"
 git add .
 echo -e "${GREEN}git add . was performed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
 eval "git commit -am \"${comment}\""
 echo -e "${GREEN}git commit -am was performed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
 git push
 echo -e "${GREEN}git push was performed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
 echo -e "${GREEN}deployment complete!${NC}"
