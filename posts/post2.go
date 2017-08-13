fc07e6f7f820772e2df14b77799beb0eb8ff2003
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-13 16:49:41 +0900

    trying to extract temperature data

--- posts/post2.go
+++ posts/post2.go
@@ -1,10 +1,31 @@
 package posts
 
 import (
-	"io/ioutil"
-	"log"
+	"encoding/json"
 	"net/http"
+	"time"
 )
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJSON(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+// WeatherReport represents a typical weather report
+type WeatherReport struct {
+	ID              int32   `json:"id,omitempty"`
+	TemperatureLow  float32 `json:"temperatureLow,omitempty"`
+	TemperatureHigh float32 `json:"temperatureHigh,omitempty"`
+	Precipitation   float32 `json:"precipitation,omitempty"`
+	Humidity        float32 `json:"humidity,omitempty"`
+	Wind            float32 `json:"wind,omitempty"`
+}
 
 func loadPost2() {
 	// `d`, the reference to the DOM is declared in a previous post
@@ -13,16 +34,7 @@ func loadPost2() {
 	d.GetElementByID("post-2-description-1").SetInnerHTML("In an attempt to get a fullstack Golang project up and running, I decided to deploy a very simple Golang REST API to Heroku, and connect it to the GopherJS frontend. Below is the relevant server-side and frontend code.")
 
 	url := "https://gopherjs-api.herokuapp.com/weatherreport"
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	println(body)
+	weatherReport := new(WeatherReport) // or &Foo{}
+	getJSON(url, weatherReport)
+	println(weatherReport.TemperatureLow)
 }
