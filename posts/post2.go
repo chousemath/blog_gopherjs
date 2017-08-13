8171666c57dfb7c084411cd109595b3f956e8d8a
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-13 16:53:55 +0900

    testing deployment again

--- posts/post2.go
+++ posts/post2.go
@@ -3,13 +3,10 @@ package posts
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 )
 
-var myClient = &http.Client{Timeout: 10 * time.Second}
-
 func getJSON(url string, target interface{}) error {
-	r, err := myClient.Get(url)
+	r, err := http.Get(url)
 	if err != nil {
 		return err
 	}
