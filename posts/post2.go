a9a8e39283668038230c58983029449c4c58d2e5
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-13 16:38:11 +0900

    again trying to extract json body

--- posts/post2.go
+++ posts/post2.go
@@ -1,36 +1,29 @@
 package posts
 
 import (
-	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
-
-// WeatherReport is identical to the struct defined in the REST API
-type WeatherReport struct {
-	ID              int32   `json:"id,omitempty"`
-	TemperatureLow  float32 `json:"temperatureLow,omitempty"`
-	TemperatureHigh float32 `json:"temperatureHigh,omitempty"`
-	Precipitation   float32 `json:"precipitation,omitempty"`
-	Humidity        float32 `json:"humidity,omitempty"`
-	Wind            float32 `json:"wind,omitempty"`
-}
-
-func getJSON(url string, target interface{}) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	// not sure what this is...
-	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(target)
-}
 
 func loadPost2() {
 	// `d`, the reference to the DOM is declared in a previous post
 	d.GetElementByID("post-2-title").SetInnerHTML("Day 2: Setting up a REST API")
 	d.GetElementByID("post-2-attribution").SetInnerHTML("By Joseph Choi under <a class='post-category post-category-js'>Go</a> <a class='post-category post-category-js'>REST API</a> <a class='post-category post-category-js'>Heroku</a>")
 	d.GetElementByID("post-2-description-1").SetInnerHTML("In an attempt to get a fullstack Golang project up and running, I decided to deploy a very simple Golang REST API to Heroku, and connect it to the GopherJS frontend. Below is the relevant server-side and frontend code.")
-	weatherReport := new(WeatherReport)
-	getJSON("https://gopherjs-api.herokuapp.com/weatherreport", weatherReport)
-	println("TemperatureLow: %s\n", weatherReport.TemperatureLow)
+
+	url := "https://gopherjs-api.herokuapp.com/weatherreport"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	println(body)
 }
