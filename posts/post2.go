f3d452a88efc0905c82ec9baf4415801a3c81595
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-13 16:55:24 +0900

    again testing deployment

--- posts/post2.go
+++ posts/post2.go
@@ -26,7 +26,7 @@ type WeatherReport struct {
 
 func loadPost2() {
 	// `d`, the reference to the DOM is declared in a previous post
-	d.GetElementByID("post-2-title").SetInnerHTML("Day 2: Connecting a Go REST API")
+	d.GetElementByID("post-2-title").SetInnerHTML("Day 2: Connecting a REST API")
 	d.GetElementByID("post-2-attribution").SetInnerHTML("By Joseph Choi under <a class='post-category post-category-js'>Go</a> <a class='post-category post-category-js'>REST API</a> <a class='post-category post-category-js'>Heroku</a>")
 	d.GetElementByID("post-2-description-1").SetInnerHTML("In an attempt to get a fullstack Golang project up and running, I decided to deploy a very simple Golang REST API to Heroku, and connect it to the GopherJS frontend. Below is the relevant server-side and frontend code.")
 
